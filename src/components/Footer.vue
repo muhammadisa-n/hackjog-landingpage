e36b0734759124afccf9cfa3bf4d199d4599d68e
muhammadisa226 <muhammadisa226@gmail.com>
2023-10-10 19:24:04 +0700

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,54 +1,64 @@
 <template>
-    <footer class="bg-gradient-to-b from-primary to-gradasi">
-        <div class="mx-auto w-full p-14 ">
-            <div class=" mb-6 md:mb-0">
-                <a href="#">
-                    <img src="../assets/images/logo.png" alt="" class="h-8 mr-3">
-                </a>
-            </div>
-            <div class="flex">
-                <span class="font-monserrat text-white flex-1 block text-sm sm:text-left w-3/4 pt-4 pb-4">Hackjog sebuah ekosistem Industri digital
-                    yang berada di D.I.Y dalam
-                    bentuk kolaborasi hexahelix.
-                    Berfokus pada <br> menumbuh kembangkan industri digital serta transformasi masyarakat
-                    melalui Artificial Intelligence. </span>
-                <ul class="flex ">
-                    <li>
-                        <a href="#"> 
-                            <img src="../assets/images/instagram-white.png" alt="Instagram Hackjog" 
-                            class= "w-6 h-6 mr-3 flex justify-center items-center  hover:bg-primary"> 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"> 
-                            <img src="../assets/images/twitter-white.png" alt="Twitter Hackjog" class= "w-6 h-6 mr-3 flex justify-center items-center  hover:bg-primary"> 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"> 
-                            <img src="../assets/images/youtube.png" alt="Youtube Hackjog" class="h-6"> 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" >
-                            <img src="" alt="">
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <hr>
-            <div>
-                <span class="block text-sm text-white sm:text-left pt-2">© 2023 - HACKJOG:DIGITAL
-                    DAEMON</span>
-            </div>
-        </div>
-    </footer>
+  <footer class="bg-gradient-to-b from-primary to-gradasi">
+    <div class="mx-auto w-full p-14">
+      <div class="mb-6 md:mb-0">
+        <a href="#">
+          <img src="../assets/images/logo.png" alt="" class="h-8 mr-3" />
+        </a>
+      </div>
+      <div class="flex">
+        <span
+          class="font-monserrat text-white flex-1 block text-sm sm:text-left w-3/4 pt-4 pb-4"
+          >Hackjog sebuah ekosistem Industri digital yang berada di D.I.Y dalam
+          bentuk kolaborasi hexahelix. Berfokus pada <br />
+          menumbuh kembangkan industri digital serta transformasi masyarakat
+          melalui Artificial Intelligence.
+        </span>
+        <ul class="flex">
+          <li>
+            <a href="#">
+              <img
+                src="../assets/images/instagram-white.png"
+                alt="Instagram Hackjog"
+                class="w-6 h-6 mr-3 flex justify-center items-center hover:bg-primary"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img
+                src="../assets/images/twitter-white.png"
+                alt="Twitter Hackjog"
+                class="w-6 h-6 mr-3 flex justify-center items-center hover:bg-primary"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img
+                src="../assets/images/youtube.png"
+                alt="Youtube Hackjog"
+                class="h-6"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <span class="block text-sm text-white sm:text-left pt-2"
+          >© 2023 - HACKJOG:DIGITAL DAEMON</span
+        >
+      </div>
+    </div>
+  </footer>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
