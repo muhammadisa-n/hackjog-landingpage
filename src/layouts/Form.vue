d9abc42b460b57546f1357a6372fb4179454d70d
RizkiBimo09 <rizkibimow@gmail.com>
2023-10-10 22:20:58 +0700

    register responsif develop

--- src/layouts/Form.vue
+++ src/layouts/Form.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <section id="about" class="absolute inset-x-0 bottom-20 p-10">
     <div class="relative text-slate-500">
       <div
@@ -45,7 +45,61 @@
       </div>
     </div>
   </section>
+</template> -->
+
+<template>
+  <section id="about" class="p-10">
+    <div class="flex flex-col items-center justify-center min-h-screen">
+      <div
+        class="bg-white rounded-lg shadow-lg w-full sm:w-[400px] lg:w-[600px] xl:w-[900px] p-10"
+      >
+        <input
+          type="text"
+          id="large-input"
+          class="block w-full p-4 text-black border border-gray-300 rounded-lg bg-gray-100 sm:text-md focus:ring-purple-500 placeholder-black mb-4"
+          placeholder="Email / Username"
+        />
+        <input
+          type="text"
+          id="large-input"
+          class="block w-full p-4 text-black border border-gray-300 rounded-lg bg-gray-100 sm:text-md focus:ring-purple-500 placeholder-black mb-4"
+          placeholder="Email / Username"
+        />
+        <input
+          type="text"
+          id="large-input"
+          class="block w-full p-4 text-black border border-gray-300 rounded-lg bg-gray-100 sm:text-md focus:ring-purple-500 placeholder-black mb-4"
+          placeholder="Email / Username"
+        />
+        <input
+          type="text"
+          id="large-input"
+          class="block w-full p-4 text-black border border-gray-300 rounded-lg bg-gray-100 sm:text-md focus:ring-purple-500 placeholder-black mb-4"
+          placeholder="Email / Username"
+        />
+        <input
+          type="text"
+          id="large-input"
+          class="block w-full p-4 text-black border border-gray-300 rounded-lg bg-gray-100 sm:text-md focus:ring-purple-500 placeholder-black mb-4"
+          placeholder="Email / Username"
+        />
+        <button
+          class="w-full rounded-lg px-4 py-2 font-black text-white ring-1 bg-primary hover:bg-white hover:text-black hover:outline-none hover:ring-2 hover:ring-primary"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+#about {
+  position: relative;
+  bottom: 300px;
+}
+</style>
+
 
 <script>
 export default {
