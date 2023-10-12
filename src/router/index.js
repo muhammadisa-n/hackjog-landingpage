cbabc14e3e83e0e2d51ba0854c85be3acc4b4bbe
muhammadisa226 <muhammadisa226@gmail.com>
2023-10-13 06:46:27 +0700

    add page not found

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import IndexView from "../views/IndexView.vue";
 import Registration from "../views/Registration.vue";
+import PageNotFound from "../views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/registration",
       name: "registration",
       component: Registration,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notfound",
+      component: PageNotFound,
     },
   ],
 });
