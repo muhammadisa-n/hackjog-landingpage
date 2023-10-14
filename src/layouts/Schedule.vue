588ae03bf96c089267e26411cab182a51800f790
muhammadisa226 <muhammadisa226@gmail.com>
2023-10-14 19:47:40 +0700

    refactoring schedule page

--- src/layouts/Schedule.vue
+++ src/layouts/Schedule.vue
@@ -125,7 +125,11 @@
 </template>
 
 <script>
-import "boxicons";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { ref } from "vue";
 export default {
   setup() {
     return {};
